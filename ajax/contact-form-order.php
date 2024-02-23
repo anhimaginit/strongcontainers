b9fed238fdc6f4e67bf4fd9a6c45335d3946d42d
anhimaginit <anh@at1ts.com>
2024-02-23 15:32:08 +0700

    ok

--- ajax/contact-form-order.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'contact-form-content.php';
-require_once 'contact-form-js1.php';
-?>
--- ajax/inc/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-$__host = dirname(dirname(__DIR__));
-
-require_once $__host . '/inc/http-request.php';
-require_once $__host . '/init.php';
-require_once $__host . '/inc/authenticate.php';
